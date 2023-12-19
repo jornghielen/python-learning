0eb34c60e49738bf63d706257b45721e4cc20e7c
Jorn Ghielen <jornghielen@MacBook-Pro.local>
2023-12-19 19:27:16 +0100

    correct spel error in motorcycles.py

--- motorcycles.py
+++ motorcycles.py
@@ -6,4 +6,4 @@ first_owned = motorcycles.pop(0)
 
 # Messages
 print(f"The last motorcycle I owned was a {last_owned.title()}.")
-print(f"The first motorcycle I owned was {first_owned.title()}.")
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
