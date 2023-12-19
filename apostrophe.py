079661c33fdaa469f9a897b496c482110cfffba0
Jorn Ghielen <jghielen@tesla.com>
2023-12-19 15:40:44 +0100

    adjusted apostrophe.py

--- apostrophe.py
+++ apostrophe.py
@@ -1,2 +1,6 @@
+# Variables
 message = "One of Python's strenghts is its diverse community."
+message2 = 'One of Pythons strenghts is its diverse community.' # Message2 only works when the ' is removed (as its between 's)
+
 print(message)
+print(message2)
