95b9d37a751fdd88c0a0368aabd04b31ce360af9
Jorn Ghielen <jghielen@tesla.com>
2023-12-19 15:42:35 +0100

    added additional line to apostrophe.py

--- apostrophe.py
+++ apostrophe.py
@@ -2,5 +2,6 @@
 message = "One of Python's strenghts is its diverse community."
 message2 = 'One of Pythons strenghts is its diverse community.' # Message2 only works when the ' is removed (as its between 's)
 
+# Printing the above messages.
 print(message)
 print(message2)
