b8854e07f3848be9e3a3c304e156e15b9b8e3f4e
Jorn Ghielen <jghielen@tesla.com>
2023-12-19 15:53:11 +0100

    Changed apostrophe.py again

--- apostrophe.py
+++ apostrophe.py
@@ -5,3 +5,4 @@ message2 = 'One of Pythons strenghts is its diverse community.' # Message2 only 
 # Printing the above messages.
 print(message)
 print(message2)
+print("One of Python's strenghts is its diverse community.") # We can also print the message like this. 
