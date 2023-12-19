87007b3ce5792bc551b993ac14f249648bd26ae0
Jorn Ghielen <jornghielen@MacBook-Pro.local>
2023-12-19 19:17:06 +0100

    added bicycles.py and motorcycles.py

---
+++ bicycles.py
@@ -0,0 +1,4 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized'] # define bicycle brands
+message = f"My first bicycle was a {bicycles[1].title()}." # define the message
+
+print(message) # Print the message.
---
+++ motorcycles.py
@@ -0,0 +1,9 @@
+motorcycles = ['honda', 'yamaha', 'suzuki'] # Owned Motorcycles
+
+# Lists
+last_owned = motorcycles.pop() 
+first_owned = motorcycles.pop(0)
+
+# Messages
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
+print(f"The first motorcycle I owned was {first_owned.title()}.")
