970ec65d0fc084d10c1d55543f3d3c862e47554e
Jorn Ghielen <jornghielen@MacBook-Pro.local>
2024-01-13 15:16:04 +0100

    PEP 8 style adjust

--- food_restaurant.py
+++ food_restaurant.py
@@ -4,7 +4,6 @@ print("During winter you can choose for the first course between:")
 for first_course in first_courses:
     print(first_course.title())
 
-
 # First course for the summer
 first_courses = ('cappacia', 'hawai sandwish', 'summer soup', 'bread with cheese', 'galic bread',)
 print("\nDuring summer you can choose for the first course between:")
