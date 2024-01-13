7b7d1de5de975643b8f00f31424ee1dcd88e30ee
Jorn Ghielen <jornghielen@MacBook-Pro.local>
2024-01-13 15:06:59 +0100

    Modified foods to include loops, created restaurants menu and dimensions

---
+++ food_restaurant.py
@@ -0,0 +1,16 @@
+# First course for the winter
+first_courses = ('cappacia', 'tomato soup', 'winter soup', 'bread with cheese', 'galic bread',)
+
+# Display the winter menu
+print("During winter you can choose for the first course between:")
+for first_course in first_courses:
+    print(first_course.title())
+
+
+# First course for the summer
+first_courses = ('cappacia', 'hawai sandwish', 'summer soup', 'bread with cheese', 'galic bread',)
+
+# Display the summer menu
+print("\nDuring summer you can choose for the first course between:")
+for first_course in first_courses:
+    print(first_course.title())
--- foods.py
+++ foods.py
@@ -5,7 +5,9 @@ my_foods.append('cannoli')
 friend_foods.append('ice cream')
 
 print("My favorite foods are:")
-print(my_foods)
+for my_food in my_foods:
+    print(my_food.title())
 
 print("\nMy friend's favorite foods are:")
-print(friend_foods)
+for friend_food in friend_foods:
+    print(friend_food.title())
