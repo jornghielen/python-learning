6e113c174618812172c2da0fd804be2a9db127d4
Jorn Ghielen <jornghielen@MacBook-Pro.local>
2024-02-25 15:07:43 +0100

    python learning feb 25

---
+++ alien_color.py
@@ -0,0 +1,8 @@
+alien_color = ['yellow']
+
+if 'green' in alien_color:
+    print("You have earned 5 points.")
+elif 'yellow' in alien_color:
+    print("You have earned 10 ponts.")
+elif 'red' in alien_color:
+    print("You have earned 15 ponts.")
--- toppings.py
+++ toppings.py
@@ -1,4 +1,10 @@
-requested_topping = 'mushrooms'
+requested_topping = ['mushrooms', 'pepperoni']
 
-if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
+if 'mushrooms' in requested_topping:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_topping:
+    print("Adding pepperoni")
+if 'extra cheese' in requested_topping:
+    print ("Adding extra cheese")
+
+print("\nFinished making your pizza!")
