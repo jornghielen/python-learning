0875dedaff86572486cc0482002a6fd10fbb1466
Jorn Ghielen <jornghielen@MacBook-Pro.local>
2024-03-16 16:21:15 +0100

    Learned again on March 16th

--- toppings.py
+++ toppings.py
@@ -1,10 +1,12 @@
-requested_topping = ['mushrooms', 'pepperoni']
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+                      'pepperoni', 'pineapple', 'extra cheese']
 
-if 'mushrooms' in requested_topping:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_topping:
-    print("Adding pepperoni")
-if 'extra cheese' in requested_topping:
-    print ("Adding extra cheese")
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
 
-print("\nFinished making your pizza!")
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+
+print(f"\nFinished making your pizza!")
