72a803ac674abc4218a93556ebbb216bc0ee4c1f
Jorn Ghielen <jornghielen@MacBook-Pro.local>
2025-02-25 15:24:10 +0100

    learning data graphs

--- Python_learning_chapter_2/Generating_data/scatter_squares.py
+++ Python_learning_chapter_2/Generating_data/scatter_squares.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
 
-plt.style.use('seaborn')
-fig, ax = plt.subplot()
-ax.scatter(2, 4, s=200)
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
-ax.set_title("Square Numbers", frontsize=24)
-ax.set_xlabel("Value", frontsize=14)
-ax.set_ylabel("Square of Value", frontsize=14)
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
 
 # Set size of tick labels.
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
