58b08f407f2b30727c2994f8fedbc8b8641a6922
Jorn Ghielen <jghielen@tesla.com>
2025-04-28 09:40:22 +0200

    testing

---
+++ Python_learning_chapter_2/downloading_data/eq_explore_data.py
@@ -0,0 +1,10 @@
+import json
+
+# Explore the structure of this data.
+filename ='Python_learning_chapter_2/downloading_data/data/eq_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+readable_file = 'Python_learning_chapter_2/downloading_data/readable_eq_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4)
