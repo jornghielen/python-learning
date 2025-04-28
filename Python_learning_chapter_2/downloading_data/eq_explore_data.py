1c03c5b4c34fd1d27cc20c3bac9a41c43f571893
Jorn Ghielen <jghielen@tesla.com>
2025-04-28 09:54:45 +0200

    not working, test at home

--- Python_learning_chapter_2/downloading_data/eq_explore_data.py
+++ Python_learning_chapter_2/downloading_data/eq_explore_data.py
@@ -5,6 +5,6 @@ filename ='Python_learning_chapter_2/downloading_data/data/eq_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = 'Python_learning_chapter_2/downloading_data/readable_eq_data.json'
+readable_file = 'Python_learning_chapter_2/downloading_data/data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
     json.dump(all_eq_data, f, indent=4)
