ff97714d21b55b1137b37229a35a2d66ec242b15
Jorn Ghielen <jghielen@tesla.com>
2025-05-12 16:50:14 +0200

    map

--- Python_learning_chapter_2/downloading_data/eq_world_map.py
+++ Python_learning_chapter_2/downloading_data/eq_world_map.py
@@ -26,6 +26,10 @@ data = [{
     'lat': lats,
     'marker':{
         'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'},
     },
 }]
 
